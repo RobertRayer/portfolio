713da8c8da5d24389ca046352001ec692049d10e
Robert Rayer <robertrayer@gmail.com>
2020-07-13 00:21:31 -0400

    - Added portfolio.blazor project - Uses same email service - Similar pages rewritten using Blazor

---
+++ portfolio.blazor/Data/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Blazor.Data
+{
+    public class ContactModel
+    {
+        [DisplayName("Email")]
+        [EmailAddress]
+        [Required]
+        public string FromAddress { get; set; }
+        [DisplayName("Name")]
+        [MaxLength(100)]
+        [Required]
+        public string FromName { get; set; }
+        [DisplayName("Subject")]
+        [MaxLength(50)]
+        [Required]
+        public string Subject { get; set; }
+        [DisplayName("Body")]
+        [MaxLength(500)]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+    }
+}
